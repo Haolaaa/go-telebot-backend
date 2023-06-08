e93a82dd5299564f06cf4c18ee13e6ad374bddaf
haolaaa <halamatg@gmail.com>
2023-06-08 14:28:22 +0400

    test

--- service/sys_site_config.go
+++ service/sys_site_config.go
@@ -42,21 +42,25 @@ func (s *SiteConfigService) DeleteSiteConfig(id int) (err error) {
 }
 
 func (s *SiteConfigService) UpdateSiteConfig(req model.SysSiteConfig) error {
-	return global.DB.Model(&model.SysSiteConfig{}).
-		Select("updated_at", "parent_name", "site_name", "site_key", "site_id", "direct_play_url", "cf_play_url", "cdn_play_url", "video_cover", "download_url").
-		Updates(map[string]interface{}{
-			"updated_at":      time.Now(),
-			"parent_name":     req.ParentName,
-			"site_name":       req.SiteName,
-			"site_key":        req.SiteKey,
-			"site_id":         req.SiteID,
-			"direct_play_url": req.DirectPlayUrl,
-			"cf_play_url":     req.CFPlayUrl,
-			"cdn_play_url":    req.CDNPlayUrl,
-			"video_cover":     req.VideoCover,
-			"download_url":    req.DownloadUrl,
-		}).
-		Error
+	return global.DB.Transaction(func(tx *gorm.DB) error {
+		var siteConfig model.SysSiteConfig
+		err := tx.Where("id = ?", req.ID).First(&siteConfig).Error
+		if err != nil {
+			return err
+		}
+		if siteConfig.SiteName != req.SiteName {
+			var siteConfig model.SysSiteConfig
+			if !errors.Is(tx.Where("site_name = ?", req.SiteName).First(&siteConfig).Error, gorm.ErrRecordNotFound) {
+				return errors.New("存在相同站点名称")
+			}
+		}
+		req.UpdatedAt = time.Now()
+		err = tx.Where("id = ?", req.ID).Updates(&req).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
 func (s *SiteConfigService) GetSiteConfigByID(id int, parentName string) (siteConfig model.SysSiteConfig, err error) {
