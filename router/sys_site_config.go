d2ce627d9cf0fce1a3811758bee174c6bb63952a
haolaaa <halamatg@gmail.com>
2023-06-08 14:25:46 +0400

    test

--- router/sys_site_config.go
+++ router/sys_site_config.go
@@ -15,7 +15,7 @@ func (s *SiteConfigRouter) InitSiteConfigRouter(Router *gin.RouterGroup) {
 		siteConfigRouter.POST("siteConfigList", baseApi.GetSiteConfigList)  // 获取站点配置列表
 		siteConfigRouter.POST("addSiteConfig", baseApi.AddSiteConfig)       // 新增站点配置
 		siteConfigRouter.POST("deleteSiteConfig", baseApi.DeleteSiteConfig) // 删除站点配置
-		siteConfigRouter.POST("SetSiteConfig", baseApi.UpdateSiteConfig)    // 设置站点配置
+		siteConfigRouter.POST("setSiteConfig", baseApi.UpdateSiteConfig)    // 设置站点配置
 		siteConfigRouter.POST("getByID", baseApi.GetSiteConfigByID)         // 根据ID获取站点配置
 	}
 }
