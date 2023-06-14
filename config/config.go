f94c3214a5494555b2387e7848bdbc57793a0929
haolaaa <halamatg@gmail.com>
2023-06-14 14:02:37 +0400

    test

--- config/config.go
+++ config/config.go
@@ -1,8 +1,9 @@
 package config
 
 type Server struct {
-	JWT   JWT   `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
-	Zap   Zap   `mapstructure:"zap" json:"zap" yaml:"zap"`
-	Timer Timer `mapstructure:"timer" json:"timer" yaml:"timer"`
-	Mysql Mysql `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
+	JWT    JWT    `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
+	Zap    Zap    `mapstructure:"zap" json:"zap" yaml:"zap"`
+	Timer  Timer  `mapstructure:"timer" json:"timer" yaml:"timer"`
+	Mysql  Mysql  `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
+	System System `mapstructure:"system" json:"system" yaml:"system"`
 }
--- core/server.go
+++ core/server.go
@@ -14,8 +14,10 @@ type server interface {
 func RunWindowServer() {
 	Router := initialize.Routers()
 
-	s := initServer(":8888", Router)
+	address := fmt.Sprintf(":%d", global.CONFIG.System.Addr)
+
+	s := initServer(address, Router)
 	time.Sleep(10 * time.Microsecond)
-	global.LOG.Info(fmt.Sprintf("%s: admin服务启动成功，端口为%s", "admin", ":8888"))
+	global.LOG.Info(fmt.Sprintf("%s: admin服务启动成功，端口为%s", "admin", address))
 	global.LOG.Error(s.ListenAndServe().Error())
 }
